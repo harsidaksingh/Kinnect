790da9829f9d6bf72190a4fefe3444d6683ee213
harmeetsingh85 <singhharmeet8595@gmail.com>
2024-05-08 17:39:48 +0530

    dark mode, light mode added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
+
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -15,8 +18,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn( 
+        font.className,
+        "bg-white dark:bg-[#313338]"
+      )}>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem={false}
+        storageKey="discord-theme"
+        >
+        {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
