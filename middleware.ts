acc5ac0fa87f933606617b43d41c7baad526f8ff
harsidaksingh <harsidak.bindra21@gmail.com>
2024-05-13 01:39:35 +0530

    Add navigation sidebar

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,9 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // const isProtectedRoute = createRouteMatcher(["/(.*)"]);
 
-export default clerkMiddleware(
-  (auth, req) => {
-    // if (isProtectedRoute(req)) auth().protect();
-  },
-  { debug: true }
-);
+export default clerkMiddleware((auth, req) => {
+  // if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
