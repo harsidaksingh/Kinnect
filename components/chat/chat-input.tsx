49d2ba84caa31375c1a19df588e756582b80091c
harmeetsingh85 <singhharmeet8595@gmail.com>
2024-05-17 21:44:39 +0530

    emoji-bar

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -6,9 +6,10 @@ import { Plus } from "lucide-react";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useModal } from "@/hooks/use-modal-store";
-// import { useRouter } from "next/navigation";
 import axios from "axios";
 import qs from "query-string";
+import { EmojiPicker } from "@/components/emoji-picker";
+import { useRouter } from "next/navigation";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -23,7 +24,7 @@ const formSchema = z.object({
 
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
   const { onOpen } = useModal();
-  // const router = useRouter();
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -42,6 +43,10 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
       });
 
       await axios.post(url, values);
+
+      form.reset();
+      router.refresh();
+      
     } catch (error) {
       console.log(error);
     }
@@ -72,6 +77,11 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                     }`}
                     {...field}
                   />
+                  <div className="absolute top-7 right-8">
+                    <EmojiPicker
+                    onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                    />
+                  </div>
                 </div>
               </FormControl>
             </FormItem>
