ae6316a218ced332e364b40c15fcdae220ebeab1
harmeetsingh85 <singhharmeet8595@gmail.com>
2024-05-18 19:30:41 +0530

    add chat message component

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils";
 import "@uploadthing/react/styles.css";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -32,7 +33,9 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
+              <QueryProvider>
               {children}
+              </QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
