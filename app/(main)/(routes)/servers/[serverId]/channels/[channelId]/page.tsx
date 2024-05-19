94e6697f991cdf9acafda36ac8bc18abecfcfb73
crystal1509 <gsbhatti2001@gmail.com>
2024-05-19 19:08:54 +0530

    add AUDIO, VIDEO CALLING & SHARE SCREEN Functionality

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -5,67 +5,76 @@ import { redirect } from "next/navigation";
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
 import { ChatMessages } from "@/components/chat/chat-messages";
-
+import { ChannelType } from "@prisma/client";
+import { MediaRoom } from "@/components/media-room";
 
 interface ChannelIdPageProps {
-    params: {
-      serverId: string;
-      channelId: string;
-    };
-  }
-const ChannelIdPage= async ({
-    params
-}: ChannelIdPageProps)=>{
-    const profile=await currentProfile();
-    if(!profile){
-        return RedirectToSignIn;
-    }
-    const channel = await db.channel.findUnique({
-        where: {
-          id: params.channelId,
-        },
-      });
-    const member = await db.member.findFirst({
-        where: {
-          serverId: params.serverId,
-          profileId: profile.id,
-        },
-      });
-    if(!channel || !member){
-        redirect("/");
-    }
-
-    return (
-        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-            <ChatHeader
-            name={channel.name}
-            serverId={channel.serverId}
-            type="channel"
-        />
-        <ChatMessages
-        member={member}
-        name={channel.name}
-        chatId={channel.id}
-        type="channel"
-        apiUrl="/api/messages"
-        socketUrl="/api/socket/messages"
-        socketQuery={{
-          channelId: channel.id,
-          serverId: channel.serverId,
-        }}
-        paramKey="channelId"
-        paramValue={channel.id}
-        />
-        <ChatInput
-          name={channel.name}
-          type="channel"
-          apiUrl="/api/socket/messages"
-          query={{
-            channelId: channel.id,
-            serverId: channel.serverId,
-          }}
-        />
-        </div>
-    );
+  params: {
+    serverId: string;
+    channelId: string;
+  };
 }
-export default  ChannelIdPage;
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return RedirectToSignIn;
+  }
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+      {channel.type === ChannelType.TEXT && (
+        <>
+          <ChatMessages
+            member={member}
+            name={channel.name}
+            chatId={channel.id}
+            type="channel"
+            apiUrl="/api/messages"
+            socketUrl="/api/socket/messages"
+            socketQuery={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
+            paramKey="channelId"
+            paramValue={channel.id}
+          />
+          <ChatInput
+            name={channel.name}
+            type="channel"
+            apiUrl="/api/socket/messages"
+            query={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
+          />
+        </>
+      )}
+      {channel.type === ChannelType.AUDIO && (
+        <MediaRoom chatId={channel.id} video={false} audio={true} />
+      )}
+      {channel.type === ChannelType.VIDEO && (
+        <MediaRoom chatId={channel.id} video={true} audio={true} />
+      )}
+    </div>
+  );
+};
+export default ChannelIdPage;
--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,6 +2,7 @@ import { Hash } from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
 import { SocketIndicator } from "@/components/socket-indicator";
 import UserAvatar from "../user-avatar";
+import { ChatVideoButton } from "./chat-video-button";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -26,6 +27,7 @@ export const ChatHeader = ({
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
       <div className="ml-auto flex items-center">
+        {type === "conversation" && <ChatVideoButton />}
         <SocketIndicator />
       </div>
     </div>
